5bc2893c76173e2dd6d342d570b11ebf1c025df9
Techjoy <930763179@qq.com>
2024-05-16 21:55:20 +0800

    Implement echo bot

--- src/main/java/bot_learn/bot/Bot.java
+++ src/main/java/bot_learn/bot/Bot.java
@@ -1,13 +1,30 @@
 package bot_learn.bot;
 
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot implements LongPollingSingleThreadUpdateConsumer {
+    private static OkHttpTelegramClient client;
+
+    public Bot() {
+        client = new OkHttpTelegramClient(System.getenv("TG_BOT_TOKEN"));
+    }
+
     @Override
     public void consume(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            System.out.println(update.getMessage().getText());
+            Message msg = update.getMessage();
+            System.out.println(msg.getText());
+            SendMessage sendMessage = new SendMessage(msg.getChatId().toString(), msg.getText());
+            try {
+                client.execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
