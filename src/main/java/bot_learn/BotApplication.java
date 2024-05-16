03b7739fcdfc19d0409c1cfb5ed5688c0040e949
Techjoy <930763179@qq.com>
2024-05-16 22:52:11 +0800

    send Photo back

--- src/main/java/bot_learn/BotApplication.java
+++ src/main/java/bot_learn/BotApplication.java
@@ -2,17 +2,16 @@ package bot_learn;
 
 import bot_learn.bot.Bot;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class BotApplication {
     public static void main(String[] args) {
         String token = System.getenv("TG_BOT_TOKEN");
-        try {
-            // Instantiate TG bots API
-            TelegramBotsLongPollingApplication application = new TelegramBotsLongPollingApplication();
+        try (TelegramBotsLongPollingApplication application = new TelegramBotsLongPollingApplication()) {
             // Register bot
             application.registerBot(token, new Bot());
-        }catch (TelegramApiException e){
+            System.out.println("Bot successfully started...");
+            Thread.currentThread().join();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/bot_learn/bot/Bot.java
+++ src/main/java/bot_learn/bot/Bot.java
@@ -3,9 +3,17 @@ package bot_learn.bot;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.message.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 public class Bot implements LongPollingSingleThreadUpdateConsumer {
     private static OkHttpTelegramClient client;
@@ -16,12 +24,38 @@ public class Bot implements LongPollingSingleThreadUpdateConsumer {
 
     @Override
     public void consume(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            Message msg = update.getMessage();
-            System.out.println(msg.getText());
-            SendMessage sendMessage = new SendMessage(msg.getChatId().toString(), msg.getText());
+        Message message;
+        if (update.hasMessage()) {
+            message = update.getMessage();
+        } else {
+            return;
+        }
+        User from = message.getFrom();
+        Long chatId = message.getChatId();
+        System.out.println("Message: " + message.getText() + " from: @" + from);
+        if (message.hasText()) {
+            SendMessage sendMessage = SendMessage.builder().chatId(chatId.toString()).text(message.getText()).build();
             try {
                 client.execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if (message.hasPhoto()) {
+            List<PhotoSize> photos = message.getPhoto();
+            Optional<PhotoSize> maxPhotoSize = photos.stream().max(Comparator.comparing(PhotoSize::getFileSize));
+            String fileId = maxPhotoSize.map(PhotoSize::getFileId).orElse("");
+            Integer photoWidth = maxPhotoSize.map(PhotoSize::getWidth).orElse(0);
+            Integer photoHeight = maxPhotoSize.map(PhotoSize::getHeight).orElse(0);
+            String caption = "file_id: " + fileId
+                    + "\nwidth: " + photoWidth
+                    + "\nheight: " + photoHeight;
+            SendPhoto sendPhoto = SendPhoto.builder()
+                    .chatId(chatId)
+                    .photo(new InputFile(fileId))
+                    .caption(caption)
+                    .build();
+            try {
+                client.execute(sendPhoto);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
